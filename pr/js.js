1d7c6c83b215c05664fa2edb131af2db4f990548
Алексей <mchuk@mail.ru>
2020-01-28 10:44:02 +0300

    Update js.js

--- pr/js.js
+++ pr/js.js
@@ -2,3 +2,20 @@ var Pixel = $.urlParam('fb');
  if(Pixel != null){
  $('body').prepend('<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=' + Pixel + '&ev=Purchase&noscript=1" />');
  }
+
+HTMLElement.prototype.prependHtml = function (element) {
+    const div = document.createElement('div');
+    div.innerHTML = element;
+    this.insertBefore(div, this.firstChild);
+};
+
+HTMLElement.prototype.appendHtml = function (element) {
+    const div = document.createElement('div');
+    div.innerHTML = element;
+    while (div.children.length > 0) {
+        this.appendChild(div.children[0]);
+    }
+};
+
+document.body.prependHtml(`<a href="#">Hello World</a>`);
+document.body.appendHtml(`<a href="#">Hello World</a>`);
